18214dc922a19df31aa5a3c20298006d55c49f13
Keith <keithle888@gmail.com>
2017-04-09 02:25:22 +0800

    Mid-way fixing dictionary

--- app/src/main/java/teamenglify/englify/MainActivity.java
+++ app/src/main/java/teamenglify/englify/MainActivity.java
@@ -335,7 +335,7 @@ public class MainActivity extends AppCompatActivity {
     public void loadLoginFragment() {
         getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_container, new LoginFragment()).addToBackStack(null).commit();
         //Initialize English-Myanmar Dictionary
-        //initializeDictionary();
+        initializeDictionary();
     }
 
     public void loadNextListing(int listingType, Object objectToLoad) {
@@ -550,7 +550,7 @@ public class MainActivity extends AppCompatActivity {
                 ap.show();
             }
         } else {
-            externalApp.setFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+            externalApp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             startActivity(externalApp);
             Toast.makeText(this, R.string.Dictionary_Load_Success, Toast.LENGTH_LONG).show();
             Intent mApp = getPackageManager().getLaunchIntentForPackage("teamenglify.englify");
