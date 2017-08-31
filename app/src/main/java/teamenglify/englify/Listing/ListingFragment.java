1057ab967bf9f92cd97e3c1bd38e0f934927d445
Keith Leow <keithle888@gmail.com>
2017-08-31 21:34:43 +0800

    29 Aug 2017 - Removed no context fragment (Redundant)

--- app/src/main/java/teamenglify/englify/Listing/ListingFragment.java
+++ app/src/main/java/teamenglify/englify/Listing/ListingFragment.java
@@ -12,10 +12,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ImageView;
 
 
-import teamenglify.englify.ContentNotAvailable;
 import teamenglify.englify.DataService.DataManager;
 import teamenglify.englify.LocalSave;
 import teamenglify.englify.Model.Conversation;
