917c9409436d2ff791512237de26a7166c495dac
Keith <keithle888@gmail.com>
2018-03-31 22:06:33 +0800

    More intermediate updates. DO NOT USE THIS COMMIT!

--- app/src/main/java/teamenglify/englify/fragments/HomeFragment.kt
+++ app/src/main/java/teamenglify/englify/fragments/HomeFragment.kt
@@ -8,11 +8,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import com.afollestad.materialdialogs.MaterialDialog
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import io.reactivex.Single
 import io.realm.Realm
+import io.realm.kotlin.where
 import kotlinx.android.synthetic.main.fragment_home.*
 import teamenglify.englify.Model.Grade
 
 import teamenglify.englify.R
+import timber.log.Timber
 
 class HomeFragment : Fragment() {
     lateinit var realm: Realm
@@ -41,11 +49,46 @@ class HomeFragment : Fragment() {
         fragment_home_recycler.adapter = GradeAdapter()
         val layoutMgr = LinearLayoutManager(activity.applicationContext, LinearLayoutManager.VERTICAL, false)
         fragment_home_recycler.layoutManager = layoutMgr
+
+        //Init random stuff
+        activity.actionBar.title = "Grades"
+
+        //Check if grades tree is downloaded.
+
+
+
     }
 
     override fun onDestroy() {
         super.onDestroy()
         realm.close()
+    }
+
+    fun getGrades(): Single<List<Grade>> {
+        val loadingDialog = MaterialDialog.Builder(activity.applicationContext)
+                .title("Loading")
+                .content("Loading textbooks...")
+                .build()
+
+        return Single.create<List<Grade>> { emitter ->
+            //Check local database
+            val grades = realm.where<Grade>().findAll()
+            if (grades.isNotEmpty()) {
+
+            } else {
+                //If not there, go to firebase and start downloading
+                FirebaseDatabase.getInstance().reference.addListenerForSingleValueEvent(object: ValueEventListener{
+                    override fun onCancelled(p0: DatabaseError?) {
+                        if (!emitter.isDisposed) emitter.onError(p0!!.toException())
+                    }
+
+                    override fun onDataChange(p0: DataSnapshot?) {
+
+                    }
+                })
+            }
+        }.doOnSubscribe{ loadingDialog.show() }
+                .doFinally { if(loadingDialog.isShowing) loadingDialog.dismiss() }
     }
 
     private class GradeAdapter: RecyclerView.Adapter<GradeViewHolder>() {
